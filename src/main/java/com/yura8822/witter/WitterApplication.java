16c146976ebaf8138d920fdb67ea17b6c345772c
yura8822 <yura8822m@gmail.com>
2019-05-01 19:14:08 +0300

    init

---
+++ src/main/java/com/yura8822/witter/WitterApplication.java
@@ -0,0 +1,13 @@
+package com.yura8822.witter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WitterApplication.class, args);
+	}
+
+}
