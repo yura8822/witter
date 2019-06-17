1b3dd154ca05b8eafb4fe1109e3d50c4c2153d01
yura8822 <yura8822m@gmail.com>
2019-06-17 21:26:59 +0300

    add profile

--- src/main/java/com/yura8822/witter/controller/UserController.java
+++ src/main/java/com/yura8822/witter/controller/UserController.java
@@ -2,54 +2,44 @@ package com.yura8822.witter.controller;
 
 import com.yura8822.witter.domain.Role;
 import com.yura8822.witter.domain.User;
-import com.yura8822.witter.repo.UserRepo;
+import com.yura8822.witter.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/user")
-@PreAuthorize("hasAuthority('ADMIN')")
 public class UserController {
     @Autowired
-    private UserRepo userRepo;
+    private UserService userService;
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping
     public String getUsers(Map<String, Object> model){
-        List<User> userList = userRepo.findAll();
+        List<User> userList = userService.findAll();
         model.put("users", userList);
         return "userList";
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @PostMapping()
     public String saveUser(@RequestParam("userId") User user,
                            @RequestParam Map<String, Object> form,
                            @RequestParam String username){
-        user.setUsername(username);
-        user.getRoles().clear();
-        Set<String> roles = Arrays.stream(Role.values()).map(Role::getAuthority)
-                .collect(Collectors.toSet());
-
-        for (String key : form.keySet()){
-            if (roles.contains(key)){
-                user.getRoles().add(Role.valueOf(key));
-            }
-        }
-        userRepo.save(user);
+        userService.saveUser(user, username, form);
         return "redirect:user";
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping("/{id}")
     public String userEditForm(@PathVariable(value = "id") Long id,
                                Map<String, Object> model){
-        model.put("user", userRepo.findByUserID(id));
+        model.put("user", userService.findByUserID(id));
         model.put("roles", Role.values());
         return "userEdit";
     }
@@ -62,5 +52,20 @@ public class UserController {
 //        return "userEdit";
 //    }
 
+    @GetMapping("/profile")
+    public String getProfile(Map<String, Object> model,
+                             @AuthenticationPrincipal User user){
+        model.put("username", user.getUsername());
+        model.put("email", user.getEmail());
+        return "profile";
+    }
 
+    @PostMapping("/profile")
+    public String updateProfile(@AuthenticationPrincipal User user,
+                                @RequestParam String username,
+                                @RequestParam String password,
+                                @RequestParam String email) {
+        userService.updateProfile(user, username, password, email);
+        return "redirect:profile";
+    }
 }
--- src/main/java/com/yura8822/witter/service/UserService.java
+++ src/main/java/com/yura8822/witter/service/UserService.java
@@ -10,8 +10,8 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
-import java.util.Collections;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -39,6 +39,11 @@ public class UserService implements UserDetailsService {
         user.setActivationCode(UUID.randomUUID().toString());
         userRepo.save(user);
 
+        sendMessage(user);
+        return true;
+    }
+
+    private void sendMessage(User user) {
         if (!StringUtils.isEmpty(user.getEmail())){
             String message = String.format("Hello %s \n" +
                     "Welcome. Please visit link: http://localhost:8080/activate/%s",
@@ -46,7 +51,6 @@ public class UserService implements UserDetailsService {
 
             mailSende.sendMail(user.getEmail(), "Activation code", message);
         }
-        return true;
     }
 
     public boolean activateUser(String uuid){
@@ -58,5 +62,56 @@ public class UserService implements UserDetailsService {
         user.setActivationCode(null);
         userRepo.save(user);
         return true;
+    }
+
+    public List<User> findAll() {
+        return userRepo.findAll();
+    }
+
+    public void saveUser(User user, String username, Map<String, Object> form) {
+        user.setUsername(username);
+        user.getRoles().clear();
+        Set<String> roles = Arrays.stream(Role.values())
+                .map(Role::getAuthority)
+                .collect(Collectors.toSet());
+
+        for (String key : form.keySet()){
+            if (roles.contains(key)){
+                user.getRoles().add(Role.valueOf(key));
+            }
+        }
+        userRepo.save(user);
+    }
+
+    public User findByUserID(Long id) {
+        return userRepo.findByUserID(id);
+    }
+
+    public void updateProfile(User user, String username, String password, String email){
+        String userEmail = user.getEmail();
+
+        boolean isEmailChanged = (email != null && !email.equals(userEmail)) ||
+                (userEmail != null && !userEmail.equals(email));
+
+        if (isEmailChanged){
+            user.setEmail(email);
+            if (!StringUtils.isEmpty(email)){
+                user.setActivationCode(UUID.randomUUID().toString());
+            }
+        }
+
+        if(!StringUtils.isEmpty(password)){
+            user.setPassword(password);
+        }
+
+        if (!StringUtils.isEmpty(username)){
+            user.setUsername(username);
+        }
+
+        userRepo.save(user);
+
+        if (isEmailChanged) {
+            sendMessage(user);
+        }
     }
 }
