76633ae5d6e8eca41698920919196c398fc0a2ef
yura8822 <yura8822m@gmail.com>
2019-07-15 21:39:22 +0300

    added My messages

--- src/main/java/com/yura8822/witter/controller/MainController.java
+++ src/main/java/com/yura8822/witter/controller/MainController.java
@@ -3,6 +3,7 @@ package com.yura8822.witter.controller;
 import com.yura8822.witter.domain.Message;
 import com.yura8822.witter.domain.User;
 import com.yura8822.witter.repo.MessageRepo;
+import com.yura8822.witter.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -19,6 +21,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.UUID;
 
 @Controller
@@ -26,6 +29,9 @@ public class MainController {
 
     @Autowired
     private MessageRepo messageRepo;
+
+    @Autowired
+    private UserRepo userRepo;
 
     @Value("${upload.patch}")
     private String uploadPath;
@@ -78,4 +84,16 @@ public class MainController {
         model.addAttribute("messages", messages);
         return "main";
     }
+
+    @GetMapping("/user-messages/{userId}")
+    public String userMessages(@AuthenticationPrincipal User currentUser,
+                               @PathVariable String userId,
+                               Model model){
+        Long userIdLong = Long.parseLong(userId);
+        User user = userRepo.findByUserID(userIdLong);
+        Set<Message> messages = user.getMessages();
+        model.addAttribute("messages", messages);
+        return "userMessages";
+    }
+
 }
--- src/main/java/com/yura8822/witter/domain/User.java
+++ src/main/java/com/yura8822/witter/domain/User.java
@@ -7,6 +7,7 @@ import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import java.util.Collection;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -33,12 +34,28 @@ public class User implements UserDetails {
     @Enumerated(EnumType.STRING)
     private Set<Role>roles;
 
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<Message> messages;
+
     @Email(message = "Email is not correct")
     private String email;
 
     private String activationCode;
 
     public User() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id.equals(user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 
     @Override
@@ -128,5 +145,13 @@ public class User implements UserDetails {
 
     public void setPasswordConfirmation(String passwordConfirmation) {
         this.passwordConfirmation = passwordConfirmation;
+    }
+
+    public Set<Message> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(Set<Message> messages) {
+        this.messages = messages;
     }
 }
